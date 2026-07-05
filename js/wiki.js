276541c8b4d89fce1521a4198ee457f5385e98a8
Jhaz <jhazmineaudrey7@gmail.com>
2026-07-05 20:34:45 +0800

    feat: add wiki folder scroll anim

--- js/wiki.js
+++ js/wiki.js
@@ -1,10 +1,28 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
+
+
+// SCROLL ANIMATIONS
+const infoText = document.querySelector("#M4FIA-INFORMATION-TITLE");
+
+const infoTextObserver = new IntersectionObserver((entry)=>{
+    if (entry[0].isIntersecting){
+        entry[0].target.classList.add("flicker");
+    }
+}, {});
+
+infoTextObserver.observe(infoText);
+
+
+
+const folders = document.querySelectorAll(".folder");
+
+const folderObserver = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add("slam");
         }
     })
-}, {})
+}, {
+    threshold: 0.3
+});
 
-const files = document.querySelectorAll('.folder');
-files.forEach((el) => observer.observe(el));
+folders.forEach((folder)=>folderObserver.observe(folder));
