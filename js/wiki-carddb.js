f834cd68af64adae8c989f579d3599b133d103e6
Jhaz <jhazmineaudrey7@gmail.com>
2026-08-15 19:07:49 +0800

    feat: add search bar functionality
    I need to add extra logic where the gradient on the searched cards are according to their faction

---
+++ js/wiki-carddb.js
@@ -0,0 +1,33 @@
+const searchBar = document.querySelector("#SEARCH-BAR");
+const cards = document.querySelectorAll(".card");
+const srcc = document.querySelector("#SEARCH-RESULTS-CARD-CONT")
+
+searchBar.addEventListener("input", (inp) => {
+    const children = Array.from(srcc.children)
+
+    if (children.length > 0) {
+        children.forEach((child) => {
+            child.remove()
+        })
+    }
+
+    const searchValue = inp.target.value.toLowerCase();
+
+    if (searchValue.trim() === "") {
+        if (children.length > 0) {
+            children.forEach((child) => {
+                child.remove()
+            })
+        }
+    } else {
+        cards.forEach((el) => {
+            const cardTitle = el.querySelector("h4");
+
+            if (cardTitle.textContent.toLowerCase().includes(searchValue)) {
+                const card = el.cloneNode(true);
+                srcc.appendChild(card)
+            };
+        });
+    }
+
+});
