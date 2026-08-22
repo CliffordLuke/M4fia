5f69d98d3d8192be1e73ef728d1e03360de3acd1
Akio1997 <timothymcquin1997@gmail.com>
2026-08-22 18:45:36 +0800

    feat: add scroll function and initial duplicating for inputs in cli
    also refactored the input check to be in a function so it's recallable

--- js/suggestions.js
+++ js/suggestions.js
@@ -25,93 +25,103 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 
 // Main CLI (Command Line Interface) function
-input.forEach((check) => { 
-    check.addEventListener('keydown', () => { 
-        if ((event.code === "Enter" && !(event.shiftKey)) || (event.code === "NumpadEnter" && !(event.shiftKey))) {
-            err.textContent = ""
-            switch (true) {
-                case divtext.classList.contains("hide"):
-                    let catval = document.getElementById("category").value;
-                    console.log(catval)
-                    if (catval >= 1 && catval <= 3 || Number.isNaN(catval)) {
-                        incat.disabled = true; 
-                        setTimeout(() => {
-                            divtext.classList.remove("hide");
-                            divtext.classList.add("show");
-                            intext.focus();
-                            switch (true) {
-                                case (Number(catval) === 1 || Number(catval) === 3):
-                                    labtext.textContent += "Suggestion> Input your suggestions below:";
-                                    break;
-
-                                case (Number(catval) === 2):
-                                    labtext.textContent += "Bug> Report your bug below:";
-                                    break;     
-                            }
-                        }, 500)
-                    } 
-                    else {
-                        err.textContent = "Invalid Category!";
-                    }
-                    break;
-
-                case divema.classList.contains("hide"):
-                    if (intext.value != "") {
-                        intext.disabled = true;
-                        setTimeout(() => {
-                            divema.classList.remove("hide");
-                            divema.classList.add("show");
-                            inema.focus();
-                        }, 500)
-                    }
-                    else {
-                        switch (true) {
-                                case (Number(catval) === 1 || Number(catval) === 3):
-                                        err.textContent = "Suggestion can not be blank!";
-                                    break;
-
-                                case (Number(catval) === 2):
-                                        err.textContent = "Bug Report can not be blank!";
-                                    break;     
-                            }
-                    }
-                    break;
-
-                case divconf.classList.contains("hide"):
-                    inema.disabled = true; 
-                    setTimeout(() => {
-                        divconf.classList.remove("hide");
-                        divconf.classList.add("show");
-                        inconf.focus();
-                    }, 500);
-                    break;
-
-                case divconf.classList.contains("show"):
-                    switch (true) {
-                        case (inconf.value === "y" || inconf.value === "Y"):
-                            incat.disabled = true;
-                            intext.disabled = true;
-                            inema.disabled = true;
-                            inconf.disabled = true
-                            err.classList.toggle("red")
-                            err.classList.toggle("green")
+function inputchecking (){
+    input.forEach((check) => { 
+        check.addEventListener('keydown', () => { 
+            if ((event.code === "Enter" && !(event.shiftKey)) || (event.code === "NumpadEnter" && !(event.shiftKey))) {
+                err.textContent = ""
+                switch (true) {
+                    case divtext.classList.contains("hide"):
+                        let catval = document.getElementById("category").value;
+                        console.log(catval)
+                        if (catval >= 1 && catval <= 3 || Number.isNaN(catval)) {
+                            incat.disabled = true; 
                             setTimeout(() => {
-                                err.textContent = "Thank you for suggesting!"    
-                            }, 500);
-                            break;
+                                divtext.classList.remove("hide");
+                                divtext.classList.add("show");
+                                intext.focus();
+                                switch (true) {
+                                    case (Number(catval) === 1 || Number(catval) === 3):
+                                        labtext.textContent += "Suggestion> Input your suggestions below:";
+                                        break;
 
-                        case (inconf.value === "n" || inconf.value === "N"):
-                            err.textContent = ""
-                            incat.disabled = false;
-                            intext.disabled = false;
-                            inema.disabled = false;
-                            break;
-                    }        
-                    break;
+                                    case (Number(catval) === 2):
+                                        labtext.textContent += "Bug> Report your bug below:";
+                                        break;     
+                                }
+                            }, 500)
+                        } 
+                        else {
+                            err.textContent = "Invalid Category!";
+                        }
+                        break;
+
+                    case divema.classList.contains("hide"):
+                        if (intext.value != "") {
+                            intext.disabled = true;
+                            setTimeout(() => {
+                                divema.classList.remove("hide");
+                                divema.classList.add("show");
+                                inema.focus();
+                            }, 500)
+                        }
+                        else {
+                            switch (true) {
+                                    case (Number(catval) === 1 || Number(catval) === 3):
+                                            err.textContent = "Suggestion can not be blank!";
+                                        break;
+
+                                    case (Number(catval) === 2):
+                                            err.textContent = "Bug Report can not be blank!";
+                                        break;
+                                }
+                        }
+                        break;
+
+                    case divconf.classList.contains("hide"):
+                        inema.disabled = true; 
+                        setTimeout(() => {
+                            divconf.classList.remove("hide");
+                            divconf.classList.add("show");
+                            inconf.focus();
+                        }, 500);
+                        break;
+
+                    case divconf.classList.contains("show"):
+                        switch (true) {
+                            case (inconf.value === "y" || inconf.value === "Y"):
+                                incat.disabled = true;
+                                intext.disabled = true;
+                                inema.disabled = true;
+                                inconf.disabled = true
+                                err.classList.toggle("red")
+                                err.classList.toggle("green")
+                                setTimeout(() => {
+                                    err.textContent = "Thank you for suggesting!"    
+                                }, 500);
+                                break;
+
+                            case (inconf.value === "n" || inconf.value === "N"):
+                                err.textContent = ""
+                                incat.disabled = false;
+                                intext.disabled = false;
+                                inema.disabled = false;
+                                let divcat2 = divcat.cloneNode(true);
+                                let divtext2 = divtext.cloneNode(true);
+                                let divema2 = divema.cloneNode(true);
+                                let divconf2 = divconf.cloneNode(true);
+                                divcat2.appendChild;
+                                divtext2.appendChild;
+                                divema2.appendChild;
+                                divconf2.appendChild;
+                                break;
+                        }        
+                        break;
+                }
             }
-        }
+        })
     })
-})
+}
 
 // Start up anim
 window.addEventListener("load", async () => {
@@ -136,5 +146,6 @@ window.addEventListener("load", async () => {
     inema.value = ""
     intext.value = ""
     inconf.value = ""
+    inputchecking()
 })
 
