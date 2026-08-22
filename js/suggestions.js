bf4641360d5d99ced8c03dd21ca97b8af72d04a2
Akio1997 <timothymcquin1997@gmail.com>
2026-08-22 15:24:02 +0800

    feat: add delays for each command line input to appear after pressing enter

--- js/suggestions.js
+++ js/suggestions.js
@@ -30,41 +30,51 @@ input.forEach((check) => {
             switch (true) {
                 case divtext.classList.contains("hide"):
                     if (incat.value >= 1 && incat.value <= 3 || Number.isNaN(incat.value)) {
-                        divtext.classList.toggle("hide");
-                        divtext.classList.toggle("show");
-                        intext.focus();
                         incat.disabled = true; 
-                        switch (true){
-                            case (Number(incat.value) === 1 || Number(incat.value) === 3):
-                                labtext.textContent += "Suggestion> Input your suggestions below:";
-                                break;
+                        setTimeout(() => {
+                            divtext.classList.remove("hide");
+                            divtext.classList.add("show");
+                            intext.focus();
 
-                            case (Number(incat.value) === 2):
-                                labtext.textContent += "Bug> Report your bug below:";
-                                break;
-                        }
-                    }
+                            switch (true) {
+                                case (Number(incat.value) === 1 || Number(incat.value) === 3):
+                                    labtext.textContent += "Suggestion> Input your suggestions below:";
+                                    break;
+
+                                case (Number(incat.value) === 2):
+                                    labtext.textContent += "Bug> Report your bug below:";
+                                    break;     
+                            }
+                        }, 500)
+                    } 
                     else {
                         err.textContent = "Invalid Category!";
                     }
                     break;
+
                 case divema.classList.contains("hide"):
                     if (intext.value != "") {
-                        divema.classList.toggle("hide");
-                        divema.classList.toggle("show");
-                        inema.focus();
                         intext.disabled = true;
+                        setTimeout(() => {
+                            divema.classList.remove("hide");
+                            divema.classList.add("show");
+                            inema.focus();
+                        }, 500)
                     }
                     else {
                         err.textContent = "Report can not be blank!";
                     }
                     break;
+
                 case divconf.classList.contains("hide"):
-                    divconf.classList.toggle("hide");
-                    divconf.classList.toggle("show");
-                    inconf.focus();
                     inema.disabled = true; 
+                    setTimeout(() => {
+                        divconf.classList.remove("hide");
+                        divconf.classList.add("show");
+                        inconf.focus();
+                    }, 500);
                     break;
+
                 case divconf.classList.contains("show"):
                     switch (true) {
                         case (inconf.value === "y" || inconf.value === "Y"):
@@ -74,19 +84,22 @@ input.forEach((check) => {
                             inconf.disabled = true
                             err.classList.toggle("red")
                             err.classList.toggle("green")
-                            err.textContent = "Thank you for suggesting!"
+                            setTimeout(() => {
+                                err.textContent = "Thank you for suggesting!"    
+                            }, 500);
                             break;
+
                         case (inconf.value === "n" || inconf.value === "N"):
                             err.textContent = "Please edit your response. When done, type y."
                             incat.disabled = false;
                             intext.disabled = false;
                             inema.disabled = false;
                             break;
-                    }
+                    }        
                     break;
-            }  
-        }          
-    })        
+            }
+        }
+    })
 })
 
 // start up anim
